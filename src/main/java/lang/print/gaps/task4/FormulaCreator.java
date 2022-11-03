5cc783a3168a87dfc469e3c41136551295b76d88
RustamKodirov <111296957+RustamKodirov@users.noreply.github.com>
2022-11-03 15:55:38 +0300

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -4,7 +4,7 @@ public class FormulaCreator {
     public static void main(String[] args) {
         int a=2;
         int b=3;
-        double ans=(9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        int ans=(9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
         System.out.println(ans);
     }
 }
